1aa807ee3d51eaf8bc2121963a81edc20bbfd178
LiuKaiLong <helloworld.lkl@gmail.com>
2017-03-28 17:34:41 +0800

    V3.2.3 添加PDF viewer 前端部分基本完成

--- src/section.vue
+++ src/section.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app4">
     <Vheader></Vheader>
-    <div class="banner-wrapper" v-if="carousel">
+    <!-- <div class="banner-wrapper" v-if="carousel">
       <Vbanner :carousel="carousel"></Vbanner>
-    </div>
+    </div> -->
     <el-row>
-      <el-col :lg="{span: 20, offset: 2}" class="recommend-wrapper border-1px">
-      	<el-col :span="20" class="pdf-viewer">
-      		<h1>This is PDF-VIEWER</h1>
+      <el-col :lg="{span: 20, offset: 2}" class="pdf-wrapper border-1px">
+      	<el-col :span="18" class="pdf-viewer">
+      		<iframe src="static/web/viewer.html?file=helloworld.pdf" style="width:100%;height:100%;"> </iframe>
       	</el-col>
-      	<el-col :span="4" class="pdf-info">
+      	<el-col :span="6" class="pdf-info">
       		<p>
       			摘要：Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque aliquam eligendi eius reiciendis minus magnam repellat nesciunt sed aspernatur numquam qui dignissimos maxime voluptatum similique eveniet minima consequatur, esse corporis.
       		</p>
@@ -23,15 +23,15 @@
 
 <script type="text/ecmascript-6">
 import header from 'components/header/header'
-import banner from 'components/banner/banner'
+// import banner from 'components/banner/banner'
 import footer from 'components/footer/footer'
 
-const ERR_OK = 0
+// const ERR_OK = 0
 
 export default {
   components: {
     'Vheader': header,
-    'Vbanner': banner,
+    // 'Vbanner': banner,
     'Vfooter': footer
   },
   data() {
@@ -46,12 +46,12 @@ export default {
     }
   },
   created() {
-    this.$http.get('/api/carousel').then((response) => {
-      response = response.data
-      if (response.errno === ERR_OK) {
-        this.carousel = response.data
-      }
-    })
+    // this.$http.get('/api/carousel').then((response) => {
+    //   response = response.data
+    //   if (response.errno === ERR_OK) {
+    //     this.carousel = response.data
+    //   }
+    // })
   }
 }
 </script>
@@ -59,6 +59,17 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus">
 @import 'common/stylus/mixin'
 #app4
-	.banner-wrapper
-		margin-bottom 20px
+	// .banner-wrapper
+	// 	margin-bottom 20px
+	.pdf-wrapper
+		margin-top 20px
+		margin-bottom 20px  
+		.pdf-viewer
+			height 700px
+		.pdf-info
+			height 700px
+			box-sizing border-box
+			padding 40px 20px
+			font-size 23px
+			background-color #fafafa
 </style>
